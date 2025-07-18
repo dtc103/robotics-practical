3211414ef4f5b15ed5f78ba4b86649e914c535ff
Jan <jan.kerner@vector.com>
2025-07-18 15:01:05 +0200

    added exercise 8 files

---
+++ exercise8/ros_ws/src/path_following/include/path_following/path_following.h
@@ -0,0 +1,14 @@
+#ifndef PATH_FOLLOWING_H
+#define PATH_FOLLOWING_H
+
+#include <rclcpp/rclcpp.hpp>
+#include "vec2.h"
+
+class PathFollowing: public rclcpp::Node {
+    public:
+        PathFollowing();
+
+    private:
+};
+
+#endif
---
+++ exercise8/ros_ws/src/path_following/src/path_following.cpp
@@ -0,0 +1,6 @@
+#include "path_following.h"
+#include "path_processing.h"
+#include "plot_data.h"
+
+PathFollowing::PathFollowing(): Node("path_following") {
+}
